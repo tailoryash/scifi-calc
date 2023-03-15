0392e1f4b016ce2cdb624ae3289b02f74b713ab0
YASH TAILOR <theyash.tailor@gmail.com>
2023-03-15 21:22:12 +0530

    fixed value in toFixed() method

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function calculate() {
 }
 
 function degree() {
-    display.value = (display.value * (180 / Math.PI)).toFixed(8);
+    display.value = (display.value * (180 / Math.PI)).toFixed(10);
 }
 
 function fixedExpo() {
@@ -74,17 +74,17 @@ function trignometry() {
 }
 
 function sin() {
-    display.value = Math.sin(display.value * Math.PI / 180).toFixed(8);
+    display.value = Math.sin(display.value * Math.PI / 180).toFixed(10);
     document.getElementById("trigno").value ="trignometry" ;
 }
 
 function cos() {
-    display.value = Math.cos(display.value * Math.PI / 180).toFixed(8);
+    display.value = Math.cos(display.value * Math.PI / 180).toFixed(10);
      document.getElementById("trigno").value ="trignometry" ;
 }
 
 function tan() {
-    display.value = Math.tan(display.value * Math.PI / 180).toFixed(8);
+    display.value = Math.tan(display.value * Math.PI / 180).toFixed(10);
     document.getElementById("trigno").value = "trignometry";
 }
 
@@ -116,7 +116,7 @@ function pi() {
     } else if (display.value == 0) {
         display.value = '0';
     } else {
-        display.value = (display.value * 3.14).toFixed(8);
+        display.value = (display.value * 3.14).toFixed(10);
     }
 }
 
@@ -125,7 +125,7 @@ function e() {
     if (display.value == "") {
         display.value = Math.E;
     } else {
-        display.value = (display.value * Math.E).toFixed(8);
+        display.value = (display.value * Math.E).toFixed(10);
     }
 }
 
@@ -150,7 +150,7 @@ function absulatevalue() {
 }
 
 function exponential() {
-    display.value = Math.exp(display.value).toFixed(8);
+    display.value = Math.exp(display.value).toFixed(10);
 }
 
 function sqroot() {
@@ -162,11 +162,11 @@ function powX() {
 }
 
 function log() {
-    display.value = Math.log10(display.value).toFixed(8);
+    display.value = Math.log10(display.value).toFixed(10);
 }
 
 function ln() {
-    display.value = Math.log(display.value).toFixed(8);
+    display.value = Math.log(display.value).toFixed(10);
 }
 
 function factorial() {
